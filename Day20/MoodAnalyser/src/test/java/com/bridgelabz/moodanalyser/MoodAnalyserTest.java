346372a75a6aa24bc8a307ac1abcee36ce9a6406
Kshitijh-A139 <akshitijhagarwal139@gmail.com>
2026-06-01 11:49:23 +0530

    add programs: [kshitijh] added MoodAnalyser and UserValidation Maven projects

--- Day20/MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ Day20/MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,4 +1,51 @@
 package com.bridgelabz.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
+
+    @Test
+    void givenSadMessage_ShouldReturnSad() {
+
+        MoodAnalyser moodAnalyser =
+                new MoodAnalyser("I am in Sad Mood");
+
+        String mood = moodAnalyser.analyseMood();
+
+        Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    void givenHappyMessage_ShouldReturnHappy() {
+
+        MoodAnalyser moodAnalyser =
+                new MoodAnalyser("I am in Happy Mood");
+
+        String mood = moodAnalyser.analyseMood();
+
+        Assertions.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    void givenNullMood_ShouldReturnHappy() {
+
+        MoodAnalyser moodAnalyser =
+                new MoodAnalyser(null);
+
+        String mood = moodAnalyser.analyseMood();
+
+        Assertions.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    void givenEmptyMood_ShouldReturnHappy() {
+
+        MoodAnalyser moodAnalyser =
+                new MoodAnalyser("");
+
+        String mood = moodAnalyser.analyseMood();
+
+        Assertions.assertEquals("HAPPY", mood);
+    }
 }
--- Day20/UserValidation/src/test/java/com/bridgelabz/uservalidation/UserValidationTest.java
+++ Day20/UserValidation/src/test/java/com/bridgelabz/uservalidation/UserValidationTest.java
@@ -1,4 +1,74 @@
 package com.bridgelabz.uservalidation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class UserValidationTest {
+
+    UserValidation validator = new UserValidation();
+
+    @Test
+    void givenValidFirstName_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidFirstName("Kshitijh"));
+    }
+
+    @Test
+    void givenValidLastName_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidLastName("Agarwal"));
+    }
+
+    @Test
+    void givenValidEmail_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidEmail("abc.xyz@gmail.com"));
+    }
+
+    @Test
+    void givenValidPhoneNumber_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidPhoneNumber("91 9876543210"));
+    }
+
+    @Test
+    void givenValidPasswordRule1_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidPasswordRule1("password"));
+    }
+
+    @Test
+    void givenValidPasswordRule2_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidPasswordRule2("Password"));
+    }
+
+    @Test
+    void givenValidPasswordRule3_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidPasswordRule3("Password1"));
+    }
+
+    @Test
+    void givenValidPasswordRule4_ShouldReturnTrue() {
+        assertEquals(true,
+                validator.isValidPasswordRule4("Password1@"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "abc@yahoo.com,true",
+            "abc.100@yahoo.com,true",
+            "abc-100@yahoo.com,true",
+            ".abc@xyz.com,false"
+    })
+    void givenEmailSamples_ShouldReturnExpectedResult(
+            String email, boolean expected) {
+
+        assertEquals(expected,
+                validator.isSampleEmailValid(email));
+    }
 }
